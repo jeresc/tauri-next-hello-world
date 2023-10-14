35799ca8e839f28cb744032e6d0eee57e938ebde
jeresc <jeremiastomassrc@gmail.com>
2023-10-13 22:16:21 -0300

    fix: 💬 change example page text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold font-serif my-6 text-center hover:scale-110 transition cursor-pointer">
-        Ojala nunca ser tan gay como Latin
+        Hey There!
       </h1>
 
       <Button />
